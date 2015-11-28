63c1f17a55d06b35a4ba6630c48b960471b6834d
Lebda <lebduska.jiri@seznam.cz>
2015-11-28 13:09:27 +0100

    Base comit

---
+++ RealityJinak/RealityJinak/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealityJinak.Startup))]
+namespace RealityJinak
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
