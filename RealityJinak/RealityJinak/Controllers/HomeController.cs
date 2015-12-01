f9fd2802008fad24c325dbeb58e942313c85847b
Lebda <lebduska.jiri@seznam.cz>
2015-12-01 16:03:07 +0100

    carousel

--- RealityJinak/RealityJinak/Controllers/HomeController.cs
+++ RealityJinak/RealityJinak/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace RealityJinak.Controllers
@@ -23,6 +21,13 @@ namespace RealityJinak.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Offers()
+        {
+            ViewBag.Message = "Offers ";
 
             return View();
         }
